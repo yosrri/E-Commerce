fbffaddf1bbcbbfa6f82fbbff3d9896e27423034
Yosri <ahmed.yosri@softxpert.com>
2024-10-22 17:28:06 +0300

    removed unneeded code

--- app/src/main/java/com/training/ecommerce/MainActivity.kt
+++ app/src/main/java/com/training/ecommerce/MainActivity.kt
@@ -3,6 +3,7 @@ package com.training.ecommerce
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
