7063b7888ad146933b3864e7d1ffab97e1b4e436
Yosri <ahmed.yosri@softxpert.com>
2024-10-22 19:48:03 +0300

    Handle Theme of versions lower than 12, and optional if you want to add animation

--- app/src/main/java/com/training/ecommerce/MainActivity.kt
+++ app/src/main/java/com/training/ecommerce/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.training.ecommerce
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
+        initSplashScreen()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+            // Add a callback that's called when the splash screen is animating to the
+            // app content.
+/*
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Create your custom animation.
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView, View.TRANSLATION_Y, 0f, -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 1000L
+
+                // Call SplashScreenView.remove at the end of your custom animation.
+                slideUp.doOnEnd { splashScreenView.remove() }
+
+                // Run your animation.
+                slideUp.start()
+            }
+*/
+        } else {
+            setTheme(R.style.Theme_ECommerce)
+        }
     }
 }
